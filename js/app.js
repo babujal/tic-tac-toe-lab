4fc35a4859f6e0a269daf35be4562d5e350b070b
babujal <dercamar1@gmail.com>
2024-06-22 22:13:43 -0400

    Reached MVP...Some css can be done

--- js/app.js
+++ js/app.js
@@ -1,8 +1,7 @@
 /*-------------------------------- Constants --------------------------------*/
 const x = 'X'
 const o = 'O'
-const propmt = document.querySelector('#message')
-// console.log(propmt)
+const prompt = document.querySelector('#message')
 const s0 = document.querySelector('#s0')
 const s1 = document.querySelector('#s1')
 const s2 = document.querySelector('#s2')
@@ -12,10 +11,7 @@ const s5 = document.querySelector('#s5')
 const s6 = document.querySelector('#s6')
 const s7 = document.querySelector('#s7')
 const s8 = document.querySelector('#s8')
-/*
-Notes for tomorrow:
-Start displaying whom turn is and who is the winner. After that, Time for CSS
-*/
+const btn = document.querySelector('button')
 
 const xMarks = []
 const oMarks = []
@@ -23,6 +19,7 @@ const oMarks = []
 
 /*---------------------------- Variables (state) ----------------------------*/
 let alternateMark = true
+let weHAveAWinner = false
 
 
 /*------------------------ Cached Element References ------------------------*/
@@ -30,95 +27,232 @@ let alternateMark = true
 
 
 /*-------------------------------- Functions --------------------------------*/
+
+const reset = () => {
+    alternateMark = true
+    weHAveAWinner = false
+    xMarks.splice(0, xMarks.length)
+    oMarks.splice(0, oMarks.length)
+    prompt.innerText = `Let's Play!`
+    s0.innerText = ''
+    s1.innerText = ''
+    s2.innerText = ''
+    s3.innerText = ''
+    s4.innerText = ''
+    s5.innerText = ''
+    s6.innerText = ''
+    s7.innerText = ''
+    s8.innerText = ''
+}
+
 const checkForWinner = (id, mark) => {
 
     if (id === 's0') {
-        if (mark===x && xMarks.includes('s1') && xMarks.includes('s2') || mark===x && xMarks.includes('s4') && xMarks.includes('s8') || mark===x && xMarks.includes('s3') && xMarks.includes('s6')) {
-            return true
-        } else if (oMarks.includes('s1') && oMarks.includes('s2') || oMarks.includes('s4') && oMarks.includes('s8') || oMarks.includes('s3') && oMarks.includes('s6')) {
-            return true
-        } else {
-            return false
+        if (mark === x) {
+            if (weHAveAWinner === false && xMarks.includes('s1') && xMarks.includes('s2') || weHAveAWinner === false && xMarks.includes('s4') && xMarks.includes('s8') || weHAveAWinner === false && xMarks.includes('s3') && xMarks.includes('s6')) {
+                prompt.innerText = 'X WINS!'
+                weHAveAWinner = true
+                return true
+            } else if (weHAveAWinner === false) {
+                prompt.innerText = 'It is O turns'
+                return false
+            }
+        }
+        if (mark === o) {
+            if (weHAveAWinner === false && oMarks.includes('s1') && oMarks.includes('s2') || weHAveAWinner === false && oMarks.includes('s4') && oMarks.includes('s8') || weHAveAWinner === false && oMarks.includes('s3') && oMarks.includes('s6')) {
+                prompt.innerText = 'O WINS!'
+                weHAveAWinner = true
+                return true
+            } else if (weHAveAWinner === false) {
+                prompt.innerText = 'It is O turns'
+                return false
+            }
         }
     }
     if (id === 's1') {
-        if (mark===x && xMarks.includes('s0') && xMarks.includes('s2') || mark===x && xMarks.includes('s4') && xMarks.includes('s7')) {
-            return true
-        } else if (oMarks.includes('s0') && oMarks.includes('s2') || oMarks.includes('s4') && oMarks.includes('s7')) {
-            return true
-        } else {
-            return false
+        if (mark === x) {
+            if (weHAveAWinner === false && xMarks.includes('s0') && xMarks.includes('s2') || weHAveAWinner === false && xMarks.includes('s4') && xMarks.includes('s7')) {
+                prompt.innerText = 'X WINS!'
+                weHAveAWinner = true
+                return true
+            } else if (weHAveAWinner === false) {
+                prompt.innerText = 'It is O turns'
+                return false
+            }
+        }
+        if (mark === o) {
+            if (weHAveAWinner === false && oMarks.includes('s0') && oMarks.includes('s2') || weHAveAWinner === false && oMarks.includes('s4') && oMarks.includes('s7')) {
+                prompt.innerText = 'O WINS!'
+                weHAveAWinner = true
+                return true
+            } else if (weHAveAWinner === false) {
+                prompt.innerText = 'It is X turns'
+                return false
+            }
         }
     }
     if (id === 's2') {
-        if (mark===x && xMarks.includes('s1') && xMarks.includes('s0') || mark===x && xMarks.includes('s4') && xMarks.includes('s6') || mark===x && xMarks.includes('s5') && xMarks.includes('s8')) {
-            return true
-        } else if (oMarks.includes('s1') && oMarks.includes('s0') || oMarks.includes('s4') && oMarks.includes('s6') || oMarks.includes('s5') && oMarks.includes('s8')) {
-            return true
-        } else {
-            return false
+        if (mark === x) {
+            if (weHAveAWinner === false && xMarks.includes('s1') && xMarks.includes('s0') || weHAveAWinner === false && xMarks.includes('s4') && xMarks.includes('s6') || weHAveAWinner === false && xMarks.includes('s5') && xMarks.includes('s8')) {
+                prompt.innerText = 'X WINS!'
+                weHAveAWinner = true
+                return true
+            } else if (weHAveAWinner === false) {
+                prompt.innerText = 'It is O turns'
+                return false
+            }
+        }
+        if (mark === o) {
+            if (weHAveAWinner === false && oMarks.includes('s1') && oMarks.includes('s0') || weHAveAWinner === false && oMarks.includes('s4') && oMarks.includes('s6') || weHAveAWinner === false && oMarks.includes('s5') && oMarks.includes('s8')) {
+                prompt.innerText = 'O WINS!'
+                weHAveAWinner = true
+                return true
+            } else if(weHAveAWinner === false) {
+                prompt.innerText = 'It is X turns'
+                return false
+            }
         }
     }
     if (id === 's3') {
-        if (mark===x && xMarks.includes('s0') && xMarks.includes('s6') || mark===x && xMarks.includes('s4') && xMarks.includes('s5')) {
-            return true
-        } else if (oMarks.includes('s0') && oMarks.includes('s6') || oMarks.includes('s4') && oMarks.includes('s5')) {
-            return true
-        } else {
-            return false
+        if (mark === x) {
+            if (weHAveAWinner === false && xMarks.includes('s0') && xMarks.includes('s6') || weHAveAWinner === false && xMarks.includes('s4') && xMarks.includes('s5')) {
+                prompt.innerText = 'X WINS!'
+                weHAveAWinner = true
+                return true
+            } else if (weHAveAWinner === false) {
+                prompt.innerText = 'It is O turns'
+                return false
+            }
+        }
+        if (mark === o) {
+            if (weHAveAWinner === false && oMarks.includes('s0') && oMarks.includes('s6') || weHAveAWinner === false && oMarks.includes('s4') && oMarks.includes('s5')) {
+                prompt.innerText = 'O WINS!'
+                weHAveAWinner = true
+                return true
+            } else if (weHAveAWinner === false) {
+                prompt.innerText = 'It is X turns'
+                return false
+            }
         }
     }
     if (id === 's4') {
-        if (mark===x && xMarks.includes('s1') && xMarks.includes('s7') || mark===x && xMarks.includes('s3') && xMarks.includes('s5') || mark===x && xMarks.includes('s0') && xMarks.includes('s8')) {
-            return true
-        } else if (oMarks.includes('s1') && oMarks.includes('s7') || oMarks.includes('s3') && oMarks.includes('s5') || oMarks.includes('s0') && oMarks.includes('s8')) {
-            return true
-        } else {
-            return false
+        if (mark === x) {
+            if (weHAveAWinner === false && xMarks.includes('s1') && xMarks.includes('s7') || weHAveAWinner === false && xMarks.includes('s3') && xMarks.includes('s5') || weHAveAWinner === false && xMarks.includes('s0') && xMarks.includes('s8') || weHAveAWinner === false && xMarks.includes('s2') && xMarks.includes('s6')) {
+                prompt.innerText = 'X WINS!'
+                weHAveAWinner = true
+                return true
+            } else if (weHAveAWinner === false) {
+                prompt.innerText = 'It is O turns'
+                return false
+            }
+        }
+        if (mark === o) {
+            if (weHAveAWinner === false && oMarks.includes('s1') && oMarks.includes('s7') || weHAveAWinner === false && oMarks.includes('s3') && oMarks.includes('s5') || weHAveAWinner === false && oMarks.includes('s0') && oMarks.includes('s8') || weHAveAWinner === false && xMarks.includes('s2') && xMarks.includes('s6')) {
+                prompt.innerText = 'O WINS!'
+                weHAveAWinner = true
+                return true
+            } else if (weHAveAWinner === false) {
+                prompt.innerText = 'It is X turns'
+                return false
+            }
         }
     }
     if (id === 's5') {
-        if (mark===x && xMarks.includes('s2') && xMarks.includes('s8') || mark===x && xMarks.includes('s4') && xMarks.includes('s3')) {
-            return true
-        } else if (oMarks.includes('s2') && oMarks.includes('s8') || oMarks.includes('s4') && oMarks.includes('s3')) {
-            return true
-        } else {
-            return false
+        if (mark === x) {
+            if (weHAveAWinner === false && xMarks.includes('s2') && xMarks.includes('s8') || weHAveAWinner === false && xMarks.includes('s4') && xMarks.includes('s3')) {
+                prompt.innerText = 'X WINS!'
+                weHAveAWinner = true
+                return true
+            } else if (weHAveAWinner === false) {
+                prompt.innerText = 'It is O turns'
+                return false
+            }
+        }
+        if (mark === o) {
+            if (weHAveAWinner === false && oMarks.includes('s2') && oMarks.includes('s8') || weHAveAWinner === false && oMarks.includes('s4') && oMarks.includes('s3')) {
+                prompt.innerText = 'O WINS!'
+                weHAveAWinner = true
+                return true
+            } else if (weHAveAWinner === false) {
+                prompt.innerText = 'It is X turns'
+                return false
+            }
         }
     }
     if (id === 's6') {
-        if (mark===x && xMarks.includes('s0') && xMarks.includes('s3') || mark===x && xMarks.includes('s4') && xMarks.includes('s2') || mark===x && xMarks.includes('s7') && xMarks.includes('s8')) {
-            return true
-        } else if (oMarks.includes('s0') && oMarks.includes('s3') || oMarks.includes('s4') && oMarks.includes('s2') || oMarks.includes('s7') && oMarks.includes('s8')) {
-            return true
-        } else {
-            return false
+        if (mark === x) {
+            if (weHAveAWinner === false && xMarks.includes('s0') && xMarks.includes('s3') || weHAveAWinner === false && xMarks.includes('s4') && xMarks.includes('s2') || weHAveAWinner === false && xMarks.includes('s7') && xMarks.includes('s8')) {
+                prompt.innerText = 'X WINS!'
+                weHAveAWinner = true
+                return true
+            } else if (weHAveAWinner === false) {
+                prompt.innerText = 'It is O turns'
+                return false
+            }
+        }
+        if (mark === o) {
+            if (weHAveAWinner === false && oMarks.includes('s0') && oMarks.includes('s3') || weHAveAWinner === false && oMarks.includes('s4') && oMarks.includes('s2') || weHAveAWinner === false && oMarks.includes('s7') && oMarks.includes('s8')) {
+                prompt.innerText = 'O WINS!'
+                weHAveAWinner = true
+                return true
+            } else if (weHAveAWinner === false) {
+                prompt.innerText = 'It is X turns'
+                return false
+            }
         }
     }
     if (id === 's7') {
-        if (mark===x && xMarks.includes('s4') && xMarks.includes('s1') || mark===x && xMarks.includes('s6') && xMarks.includes('s8')) {
-            return true
-        } else if (oMarks.includes('s4') && oMarks.includes('s1') || oMarks.includes('s6') && oMarks.includes('s8')) {
-            return true
-        } else {
-            return false
+        if (mark === x) {
+            if (weHAveAWinner === false && xMarks.includes('s4') && xMarks.includes('s1') || weHAveAWinner === false && xMarks.includes('s6') && xMarks.includes('s8')) {
+                prompt.innerText = 'X WINS!'
+                weHAveAWinner = true
+                return true
+            } else if (weHAveAWinner === false) {
+                prompt.innerText = 'It is O turns'
+                return false
+            }
+        }
+        if (mark === o) {
+            if (weHAveAWinner === false && oMarks.includes('s4') && oMarks.includes('s1') || weHAveAWinner === false && oMarks.includes('s6') && oMarks.includes('s8')) {
+                prompt.innerText = 'O WINS!'
+                weHAveAWinner = true
+                return true
+            } else if (weHAveAWinner === false) {
+                prompt.innerText = 'It is X turns'
+                return false
+            }
         }
     }
     if (id === 's8') {
-        if (mark===x && xMarks.includes('s4') && xMarks.includes('s0') || mark===x && xMarks.includes('s7') && xMarks.includes('s6') || mark===x && xMarks.includes('s5') && xMarks.includes('s2')) {
-            return true
-        } else if (oMarks.includes('s4') && oMarks.includes('s0') || oMarks.includes('s7') && oMarks.includes('s6') || oMarks.includes('s5') && oMarks.includes('s2')) {
-            return true
-        } else {
-            return false
+        if (mark === x) {
+            if (weHAveAWinner === false && xMarks.includes('s4') && xMarks.includes('s0') || weHAveAWinner === false && xMarks.includes('s7') && xMarks.includes('s6') || weHAveAWinner === false && xMarks.includes('s5') && xMarks.includes('s2')) {
+                prompt.innerText = 'X WINS!'
+                weHAveAWinner = true
+                return true
+            } else if (weHAveAWinner === false) {
+                prompt.innerText = 'It is O turns'
+                return false
+            }
+        }
+        if (mark === o) {
+            if (weHAveAWinner === false && oMarks.includes('s4') && oMarks.includes('s0') || weHAveAWinner === false && oMarks.includes('s7') && oMarks.includes('s6') || weHAveAWinner === false && oMarks.includes('s5') && oMarks.includes('s2')) {
+                prompt.innerText = 'O WINS!'
+                weHAveAWinner = true
+                return true
+            } else if (weHAveAWinner === false) {
+                prompt.innerText = 'It is X turns'
+                return false
+            }
         }
     }
 }
 
 const createMark = (char, target) => {
-    const mark = document.createElement('h3')
-    mark.textContent = char
-    target.appendChild(mark)
+    if (weHAveAWinner === false){
+        const mark = document.createElement('p')
+        mark.textContent = char
+        target.appendChild(mark)
+    }
 }
 
 const handleClick = (e) => {
@@ -128,17 +262,21 @@ const handleClick = (e) => {
     if (alternateMark && !e.target.innerText) {
         mark = x
         xMarks.push(elId)
-        console.log(checkForWinner(elId, mark))
+        createMark(mark, target)
+        checkForWinner(elId, mark)
         console.log(`Marks pushed on X ${xMarks}`)
         alternateMark = false
     } else if (!alternateMark && !e.target.innerText) {
         mark = o
         oMarks.push(elId)
-        console.log(checkForWinner(elId, mark))
+        createMark(mark, target)
+        checkForWinner(elId, mark)
         console.log(`Marks pushed on O ${oMarks}`)
         alternateMark = true
     }
-    createMark(mark, target)
+    if (weHAveAWinner === false && xMarks.length === 5) {
+        prompt.innerText = 'It is a Draw!'
+    }
 }
 
 /*----------------------------- Event Listeners -----------------------------*/
@@ -152,4 +290,5 @@ s5.addEventListener('click', handleClick)
 s6.addEventListener('click', handleClick)
 s7.addEventListener('click', handleClick)
 s8.addEventListener('click', handleClick)
+btn.addEventListener('click', reset)
 
